a8f0c50b091e8ca8a650543f9d9e6af467fffda0
Jack.Jarvis <jack.jarvis@red-gate.com>
2023-10-25 15:08:55 +0100

    Test multiple versions

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,17 +5,21 @@ namespace Tests;
 
 public class Tests
 {
-    [Test]
-    public async Task Test1()
+    private static IEnumerable<TestCaseData> TestCases()
     {
-        var container = new PostgreSqlBuilder().Build();
+        yield return new TestCaseData(new PostgreSqlBuilder().WithImage("postgres:10").Build()).SetName("Postgres v10");
+        yield return new TestCaseData(new PostgreSqlBuilder().WithImage("postgres:15").Build()).SetName("Postgres v15");
+        yield return new TestCaseData(new PostgreSqlBuilder().WithImage("postgres:16").Build()).SetName("Postgres v16");
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public async Task Test1(PostgreSqlContainer container)
+    {
         await container.StartAsync();
-
         await using var connection = new NpgsqlConnection(container.GetConnectionString());
         await connection.OpenAsync();
         await using var command = new NpgsqlCommand("SELECT 1", connection);
         await command.ExecuteScalarAsync();
-
         await container.StopAsync();
     }
 }
